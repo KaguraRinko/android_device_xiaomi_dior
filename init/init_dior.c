7c6a9672d8b6b6e0bea79220e34b67bd0c678d6f
audahadi <wan.audahadi@gmail.com>
2015-10-30 16:12:08 +0800

    init : remove prop for telephony, declared in init.dior.rc

--- init/init_dior.c
+++ init/init_dior.c
@@ -93,12 +93,10 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     /* HM NOTE LTE  */
     if (raw_id==2328) {
         property_set("ro.product.model", "HM NOTE 1LTE");
-        property_set("ro.telephony.default_network", "0,1");
     }
 
     /* HM NOTE LTE FDD */
     else {
-        property_set("ro.product.model", "HM NOTE 1LTE FDD");
-        property_set("ro.telephony.default_network", "7,1");
+        property_set("ro.product.model", "HM NOTE 1LTE TD");
     }
 }
