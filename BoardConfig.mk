e8a8716f7b779fc5452b7f9aa529d99c2b5d73fa
Faheem Kamal <opodartho@gmail.com>
2015-11-30 18:14:25 +0600

    initial changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,7 @@ TARGET_LIBINIT_DEFINES_FILE := device/xiaomi/dior/init/init_dior.cpp
 # Kernel
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 TARGET_KERNEL_SOURCE := kernel/xiaomi/dior
-TARGET_KERNEL_CONFIG := dior_custom_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_dior_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=dior user_debug=31 msm_rtb.filter=0x37 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
