99dbd7e0e3514a2204f587cfe4e0be05eb9b400c
audahadi <wan.audahadi@gmail.com>
2015-12-27 09:15:37 +0800

    update bluetooth config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,13 +17,13 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
 #define BTM_DEF_LOCAL_NAME   "HM NOTE LTE"
-
-#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_L2CAP_CHANNELS    16
-// Skips conn update at conn completion
+#define BLE_VND_INCLUDED   TRUE
+// skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_PERIPHERAL_ADV_NAME  FALSE
+#define BT_CLEAN_TURN_ON_DISABLED 1
 #endif
