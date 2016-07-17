d6426b399f2a2dedbd2d09ba483760e17d03341b
SubDragonzj <SubDragonzj@139.com>
2016-07-17 23:18:46 +0800

    dior: update init_dior: update and Switch to standalone libinit
    The kernel sets ro.boot.modem for both variants of Xiaomi Redmi Note 4G
    (LTEW and LTETD), so use that instead of reading the id from a file.
    
    ALSO No need to build the msm one
    
    Change-Id: Iece0f4b535477bcb36ba774271e0212748f7756a

--- init/init_dior.cpp
+++ init/init_dior.cpp
@@ -1,6 +1,5 @@
 /*
    Copyright (c) 2015, The Linux Foundation. All rights reserved.
-
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -13,7 +12,6 @@
     * Neither the name of The Linux Foundation nor the names of its
       contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
-
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
@@ -28,73 +26,36 @@
  */
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
 
-#include "init_msm.h"
+#define ISMATCH(a,b)    (!strncmp(a,b,PROP_VALUE_MAX))
 
-#define RAW_ID_PATH     "/sys/devices/system/soc/soc0/raw_id"
-#define BUF_SIZE         64
-static char tmp[BUF_SIZE];
-
-static int read_file2(const char *fname, char *data, int max_size)
-{
-    int fd, rc;
-
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        ERROR("failed to open '%s'\n", fname);
-        return 0;
-    }
-
-    rc = read(fd, data, max_size - 1);
-    if ((rc > 0) && (rc < max_size))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
-
-    return 1;
-}
-
-void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
+void vendor_load_properties()
 {
     char platform[PROP_VALUE_MAX];
+    char modem[PROP_VALUE_MAX];
     int rc;
-    unsigned long raw_id = -1;
-
-    UNUSED(msm_id);
-    UNUSED(msm_ver);
-    UNUSED(board_type);
 
     rc = property_get("ro.board.platform", platform);
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
         return;
 
-    /* get raw ID */
-    rc = read_file2(RAW_ID_PATH, tmp, sizeof(tmp));
-    if (rc) {
-        raw_id = strtoul(tmp, NULL, 0);
+    property_get("ro.boot.modem", modem);
+
+    if (strstr(modem, "LTEW")) {
+        property_set("ro.product.model", "HM NOTE 1LTE");
+    }
+
+    else if (strstr(modem, "LTETD")) {
+        property_set("ro.product.model", "HM NOTE 1LTE TD");
     }
 
     property_set("ro.product.device", "dior");
     property_set("ro.build.product", "dior");
-    property_set("ro.build.description", "dior-user 4.4.4 KTU84P 5.9.16 release-keys");
-    property_set("ro.build.fingerprint", "Xiaomi/dior/dior:4.4.4/KTU84P/5.9.16:user/release-keys");
-
-    /* LTE */
-    if (raw_id==2328) {
-        property_set("ro.product.model", "HM NOTE 1LTE");
-    }
-    else {
-        property_set("ro.product.model", "HM NOTE 1LTE TD");
-    }
+    property_set("ro.build.description", "dior-user 4.4.4 KTU84P V7.5.1.0.KHIMIDE release-keys");
+    property_set("ro.build.fingerprint", "Xiaomi/dior/dior:4.4.4/KTU84P/V7.5.1.0.KHIMIDE:user/release-keys");
 }
