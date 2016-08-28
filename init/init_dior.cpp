96bc82b434694f4ce1a1af2da3229cacc18a170f
Ashish Kotnala <ashishkotnala29@users.noreply.github.com>
2016-08-28 21:13:55 +0530

    update desc & fingerprint to miui8 stable

--- init/init_dior.cpp
+++ init/init_dior.cpp
@@ -58,6 +58,6 @@ void vendor_load_properties()
 
     property_set("ro.product.device", "dior");
     property_set("ro.build.product", "dior");
-    property_set("ro.build.description", "dior-user 4.4.4 KTU84P V7.5.1.0.KHIMIDE release-keys");
-    property_set("ro.build.fingerprint", "Xiaomi/dior/dior:4.4.4/KTU84P/V7.5.1.0.KHIMIDE:user/release-keys");
+    property_set("ro.build.description", "dior-user 4.4.4 KTU84P V8.0.1.0.KHIMIDG release-keys");
+    property_set("ro.build.fingerprint", "Xiaomi/dior/dior:4.4.4/KTU84P/V8.0.1.0.KHIMIDG:user/release-keys");
 }
