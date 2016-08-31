6815c3e17432da2d657ee4d030f8c5a5d996339d
Ashish Kotnala <ashishkotnala1.ak@gmail.com>
2016-08-31 22:43:17 +0530

    wifi: moved config files from kernel
    Also, WCNSS: Enable TDLS auto mode

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -35,7 +35,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := WCNSS_cfg.dat
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := ../../../../$(TARGET_KERNEL_SOURCE)/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/firmware/wlan/prima
 include $(BUILD_PREBUILT)
 
@@ -43,7 +43,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := WCNSS_qcom_cfg.ini
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := ../../../../$(TARGET_KERNEL_SOURCE)/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
 
