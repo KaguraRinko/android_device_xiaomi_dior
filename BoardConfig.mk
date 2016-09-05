74e64929607117ae2076cbe6e9b014dd31b99196
subdragonzj <zj502407679@gmail.com>
2016-09-06 00:09:03 +0800

    dior: sepolicy:disable temp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,7 +128,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
