1f568dd594bce014feebf60e37bb2a4e31eef589
SubDragonzj <SubDragonzj@139.com>
2016-10-12 18:47:56 +0800

    dior: Enable HAL3
    Change-Id: If3ae5ccb6924a3a8d59722a91be0a13006be5e65

--- device.mk
+++ device.mk
@@ -65,9 +65,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qualcomm.cabl=0
 
 # Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1
-
 PRODUCT_PACKAGES += \
     Snap \
     libxml2 \
