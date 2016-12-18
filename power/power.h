6bf048b647a760598274f69d8294679d5e9cddac
subdragonzj <zj502407679@gmail.com>
2016-12-18 21:36:30 +0800

    dior: powerHAL: update power profiles
    Change-Id: Ief110e69bd4016b65ca2677bfd3d0a6fe655a271

--- power/power.h
+++ power/power.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,17 +60,17 @@ static power_profile profiles[PROFILE_MAX] = {
     [PROFILE_BALANCED] = {
         .boost = 0,
         .boostpulse_duration = 60000,
-        .go_hispeed_load = 70,
+        .go_hispeed_load = 50,
         .go_hispeed_load_off = 90,
         .hispeed_freq = 998400,
         .hispeed_freq_off = 787200,
-        .io_is_busy = 0,
+        .io_is_busy = 1,
         .min_sample_time = 60000,
         .sampling_down_factor = 100000,
         .target_loads = "80 998400:90 1401600:99",
         .target_loads_off = "95 1401600:99",
-        .scaling_max_freq = 1401000,
-        .scaling_min_freq = 300000,
+        .scaling_max_freq = 1401600,
+        .scaling_min_freq = 787200,
         .scaling_min_freq_off = 300000,
     },
     [PROFILE_HIGH_PERFORMANCE] = {
@@ -79,7 +79,7 @@ static power_profile profiles[PROFILE_MAX] = {
          * to avoid unneccessary writes to boostpulse */
         .boostpulse_duration = 0,
         .go_hispeed_load = 50,
-        .go_hispeed_load_off = 70,
+        .go_hispeed_load_off = 50,
         .hispeed_freq = 998400,
         .hispeed_freq_off = 998400,
         .io_is_busy = 1,
@@ -88,15 +88,15 @@ static power_profile profiles[PROFILE_MAX] = {
         .target_loads = "80",
         .target_loads_off = "80",
         .scaling_max_freq = 1593600,
-        .scaling_min_freq = 998400,
+        .scaling_min_freq = 787200,
         .scaling_min_freq_off = 300000,
     },
     [PROFILE_BIAS_POWER_SAVE] = {
         .boost = 0,
-        .boostpulse_duration = 0,
+        .boostpulse_duration = 60000,
         .go_hispeed_load = 90,
         .go_hispeed_load_off = 90,
-        .hispeed_freq = 787200,
+        .hispeed_freq = 998400,
         .hispeed_freq_off = 787200,
         .io_is_busy = 0,
         .min_sample_time = 60000,
