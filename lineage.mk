c9eba2dbdcfd8d19ea76b79a28b58de59994ca7c
subdragonzj <zj502407679@gmail.com>
2017-09-05 22:06:04 +0800

    dior: Get the long list of APNs

--- lineage.mk
+++ lineage.mk
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Get the long list of APNs
+PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
