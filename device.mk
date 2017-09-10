c70b9e1ae00d61d8aa37a4262f83964931094e43
subdragonzj <zj502407679@gmail.com>
2017-09-10 16:22:19 +0800

    introduce kernel_loggy
    *  WITH_EXTRA_DBG=true

--- device.mk
+++ device.mk
@@ -271,6 +271,11 @@ PRODUCT_PACKAGES += \
     init.dior.usb.rc \
     ueventd.dior.rc
 
+ifeq ($(WITH_EXTRA_DBG),true)
+PRODUCT_PACKAGES += \
+    kernel_loggy.sh
+endif
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
