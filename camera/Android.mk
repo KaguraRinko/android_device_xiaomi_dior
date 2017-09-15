f23261d84828a16a24d17779d55964141704bf25
subdragonzj <zj502407679@gmail.com>
2017-09-15 17:56:25 +0800

    dior: camera: update for O

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,5 +1,4 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
@@ -8,15 +7,11 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES := \
     system/media/camera/include
 
-LOCAL_STATIC_LIBRARIES := libbase libarect
-
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libdl libgui \
-    android.hidl.token@1.0-utils \
+    libhardware liblog libcamera_client libgui libutils libbase
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.msm8226
-
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
