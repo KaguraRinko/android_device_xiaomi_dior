cd914511da9f502cd79955b4fbfee5b89831412f
subdragonzj <zj502407679@gmail.com>
2017-10-09 22:32:01 +0800

    dior: camera: update dependencies for O

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,17 +1,31 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_C_INCLUDES := \
+    framework/native/include \
     system/media/camera/include
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libgui libutils libbase
+    libhardware \
+    libbase \
+    libhidltransport \
+    liblog \
+    libcamera_client \
+    libutils \
+    libcutils \
+    libgui \
+    android.hidl.token@1.0-utils
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.msm8226
+
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
