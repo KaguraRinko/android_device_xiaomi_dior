c8bffaa31abe5b0e32786c3416f4be85cf209f8f
Hemant Sharma <hemantbeast@gmail.com>
2017-11-05 00:04:34 +0530

    dior: camera: Update CameraWrapper

--- camera/Android.mk
+++ camera/Android.mk
@@ -18,10 +18,13 @@ LOCAL_SHARED_LIBRARIES := \
     libutils \
     libcutils \
     libgui \
-    android.hidl.token@1.0-utils
+	libdl \
+    android.hidl.token@1.0-utils \
+	android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_STATIC_LIBRARIES := \
-    libarect
+    libarect \
+	libbase
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_PROPRIETARY_MODULE := true
