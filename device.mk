01438f012fab2da3e248fe6fae08887725538de6
subdragonzj <zj502407679@gmail.com>
2017-12-15 21:34:44 +0800

    dior: Remove unused broadcastradio hidl hal

--- device.mk
+++ device.mk
@@ -43,7 +43,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.primary.msm8226 \
