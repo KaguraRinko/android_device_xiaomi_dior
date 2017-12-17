6cb8171293053cdbdd08cafc3f2a900fbec74127
Bruno Martins <bgcngm@gmail.com>
2017-12-17 19:26:25 +0530

    dior: Add compatiblity matrix
    Change-Id: Ibf6053f1a7e9df5384664379772cb8757e897622

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,9 @@ TARGET_NO_RPC := true
 # Include
 TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
 
+# HIDL
+DEVICE_MATRIX_FILE := $(LOCAL_PATH)/compatibility_matrix.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_dior
 TARGET_RECOVERY_DEVICE_MODULES := libinit_dior
