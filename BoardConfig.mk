b06c9460ea5b88f18d3e89d1fde3d0e4c3520c7c
subdragonzj <zj502407679@gmail.com>
2017-12-31 00:07:55 +0800

    dior: change HIDL manifest definition

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,7 @@ TARGET_NO_RPC := true
 TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
 
 # HIDL
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(LOCAL_PATH)/compatibility_matrix.xml
 
 # Init
--- device.mk
+++ device.mk
@@ -162,10 +162,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl
 
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
-
 # IPC router
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
