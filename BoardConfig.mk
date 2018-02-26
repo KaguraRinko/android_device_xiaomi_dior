d06f57d6a6fa40b290a8916aae1886de4f0cada3
subdragonzj <zj502407679@gmail.com>
2018-02-26 23:17:42 +0800

    dior: Transition to TARGET_LD_SHIM_LIBS
     * Limits security exposure from shims
    
     * As a bonus, we no longer need noatsecure
       to make LD_SHIM_LIBS persist through services
    
    Change-Id: Ib84be6db70cfce1a4322ccd8a2cbd5b22e258bae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,6 +161,9 @@ include device/qcom/sepolicy/sepolicy.mk
 include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
+# Shims
+TARGET_LD_SHIM_LIBS := /system/vendor/lib/hw/camera.vendor.msm8226.so|libshim_camera.so
+
 # Odex
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
