9e8b38c8efc69439b0192199765eab3a0b0c3bbd
subdragonzj <zj502407679@gmail.com>
2018-07-31 20:56:53 +0800

    dior: bdaddr move to vendor
    Change-Id: I28f93e72a34c7359e6a0c17e4928ecb8f48c1ddd

--- bdaddr_xiaomi/Android.mk
+++ bdaddr_xiaomi/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE           := bdaddr_xiaomi
 LOCAL_MODULE_TAGS      := optional
+LOCAL_MODULE_PATH      := $(TARGET_OUT_VENDOR_EXECUTABLES)
 LOCAL_SRC_FILES        := bdaddr_xiaomi.c
 LOCAL_CFLAGS           += -Wall
 LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog libqminvapi
