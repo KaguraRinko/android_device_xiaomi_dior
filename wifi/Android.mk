f2b2df3da126d9c272e97f601794d3bd72bcb0ee
subdragonzj <zj502407679@gmail.com>
2018-07-31 21:01:39 +0800

    dior: wifi: Android.mk: use $(TARGET_OUT_ETC)
    Change-Id: Iab504e0dd59e8fb8c47579a4bb6699ed131d659c

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE       := hostapd_default.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := system/vendor/etc/hostapd
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -15,7 +15,7 @@ LOCAL_MODULE       := wpa_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := system/vendor/etc/wifi
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -23,20 +23,20 @@ LOCAL_MODULE       := p2p_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := system/vendor/etc/wifi
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
 
 # Create symbolic links
-$(shell mkdir -p system/vendor/firmware/wlan/prima; \
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
 	ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-	system/vendor/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+	$(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := WCNSS_qcom_wlan_nv_h3gbl.bin
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := system/vendor/firmware/wlan/prima
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/firmware/wlan/prima
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -44,7 +44,7 @@ LOCAL_MODULE       := WCNSS_qcom_wlan_nv_h3td.bin
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := system/vendor/firmware/wlan/prima
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/firmware/wlan/prima
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -52,11 +52,11 @@ LOCAL_MODULE       := WCNSS_qcom_wlan_nv_h3w.bin
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := system/vendor/firmware/wlan/prima
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/firmware/wlan/prima
 include $(BUILD_PREBUILT)
 
 # Make a symlink for WiFi firmware
-SYMLINKS := system/vendor/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+SYMLINKS := $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 $(SYMLINKS): WIFI_FW := WCNSS_qcom_wlan_nv_h3gbl.bin
 $(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
 	@echo "Symlink: $@ -> $(WIFI_FW)"
@@ -70,7 +70,7 @@ ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
     $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
 
 # Make a symlink for WiFi firmware
-SYMLINKS := system/vendor/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+SYMLINKS := $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 $(SYMLINKS): WIFI_FW := WCNSS_qcom_wlan_nv_h3td.bin
 $(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
 	@echo "Symlink: $@ -> $(WIFI_FW)"
@@ -84,7 +84,7 @@ ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
     $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
 
 # Make a symlink for WiFi firmware
-SYMLINKS := system/vendor/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+SYMLINKS := $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 $(SYMLINKS): WIFI_FW := WCNSS_qcom_wlan_nv_h3w.bin
 $(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
 	@echo "Symlink: $@ -> $(WIFI_FW)"
