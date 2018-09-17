8b0393dbc796d21fc14d0097256a4d6dd1be27ce
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    dior: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- device.mk
+++ device.mk
@@ -247,7 +247,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti
 
 # Rootdir
 PRODUCT_PACKAGES += \
