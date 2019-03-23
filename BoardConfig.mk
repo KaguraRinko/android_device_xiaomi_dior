a49cd6ff7d6463398bc87815d5bac804b3dd765a
subdragonzj <zj502407679@gmail.com>
2019-03-23 21:12:25 +0800

    dior: camera shim to camera.vendor.msm8226.so

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,7 +178,7 @@ BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/vendor/lib/hw/camera.msm8226.so|libshim_camera.so \
+    /system/vendor/lib/hw/camera.vendor.msm8226.so|libshim_camera.so \
     /system/vendor/lib/libFaceProc.so|libshim_dso_handle.so
 
 # Snapdragon LLVM
