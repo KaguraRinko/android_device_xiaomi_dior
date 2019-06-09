9d69b5297b058f3bad7a438bb9ab2ff3542579a1
Paul Keith <javelinanddart@gmail.com>
2019-06-09 16:39:09 +0200

    dior: Build livedisplay sysfs service
     * Add supported interfaces to manifest while we're at it,
       our panels have limited support, the only one working is
       IDisplayColorCalibration
    
    Change-Id: Iaad13fcdfb603d144599133339f9c0c6a0d1c66a

--- device.mk
+++ device.mk
@@ -202,6 +202,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8226
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
