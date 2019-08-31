01f317d9d5669dfedf9402a4aec747d107aa2710
subdragonzj <zj502407679@gmail.com>
2019-08-31 22:42:40 +0800

    dior: disable dexpreopt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,15 +70,8 @@ BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 BOARD_CHARGER_DISABLE_INIT_BLANK :=true
 WITH_MOKEE_CHARGER := false
 
-# Dexpreopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := false
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
+# dex-preopt
+WITH_DEXPREOPT := false
 
 # Display
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
