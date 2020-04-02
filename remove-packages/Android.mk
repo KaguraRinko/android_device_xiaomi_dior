1a5b122f5e8bd3c49e9f9ee4216ca21eba92e889
baolong24 <1667375308@qq.com>
2020-04-02 12:08:08 +0000

    dior: remove more apps

--- remove-packages/Android.mk
+++ remove-packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES :=  SetupWizardOverlay SetupWizard MusicFX NexusWallpapersStubPrebuilt2018 CalculatorGooglePrebuilt CalendarGooglePrebuilt Chrome Photos talkback GoogleFeedback RecorderPrebuilt Velvet WellbeingPrebuilt PixelLiveWallpaperPrebuilt WallpapersBReel2019 GoogleTTS Terminal LatinIMEGooglePrebuilt PrebuiltBugle GoogleContacts GoogleDialer WallpaperPickerGooglePrebuilt Recorder Shell PrebuiltDeskClockGoogle UpdatesBlack UpdatesDark 
+LOCAL_OVERRIDES_PACKAGES := SoundPickerPrebuilt Snap MusicFX NexusWallpapersStubPrebuilt2018 CalculatorGooglePrebuilt CalendarGooglePrebuilt Chrome Photos talkback GoogleFeedback RecorderPrebuilt Velvet WellbeingPrebuilt PixelLiveWallpaperPrebuilt WallpapersBReel2019 GoogleTTS Terminal LatinIMEGooglePrebuilt PrebuiltBugle GoogleContacts GoogleDialer WallpaperPickerGooglePrebuilt Recorder Shell PrebuiltDeskClockGoogle UpdatesBlack UpdatesDark 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
