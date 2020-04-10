250e93cd0f9936899b26f2ed50f0d3f94d887c32
baolong24 <1667375308@qq.com>
2020-04-10 09:51:19 +0800

    dior: Switch to AOSP Vibrant HAL

--- device.mk
+++ device.mk
@@ -322,7 +322,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.lineage
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # Wifi
 PRODUCT_PACKAGES += \
