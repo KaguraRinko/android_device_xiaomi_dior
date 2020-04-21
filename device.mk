f5c91c60aee15e03e0f7d2aa93d37623588d7ecf
115ek <e.blosz@hotmail.de>
2020-04-21 19:56:06 +0200

    dior: Build common_mini_phone
    * building common_mini_phone instead of common_full_phone saves use some more space to fit gapps
    * keep building the screenrecorder
    
    Change-Id: Iae3bb3e9f40151f7e7572ccaa6d8f5b2012723ee

--- device.mk
+++ device.mk
@@ -274,6 +274,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Screenrecorder
+PRODUCT_PACKAGES += \
+    Recorder
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
