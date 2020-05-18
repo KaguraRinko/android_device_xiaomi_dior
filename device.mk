1aae85bb6cdd240dabbf2ab8800fbd6870fe8d42
Arne Coucheron <arco68@gmail.com>
2020-05-18 06:29:20 +0200

    dior: Build common legacy gnss hal
    Change-Id: I56095b9c3bba065779c90b6aeb61ebfed6af7e77

--- device.mk
+++ device.mk
@@ -142,8 +142,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+    android.hardware.gnss@1.0-impl.legacy \
+    android.hardware.gnss@1.0-service.legacy
 
 # GPS
 PRODUCT_PACKAGES += \
