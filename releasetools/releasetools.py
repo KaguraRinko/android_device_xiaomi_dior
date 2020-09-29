4d80a65766b6a95e3ebfd67f8afd41f2f075d0d6
Arne Coucheron <arco68@gmail.com>
2020-09-29 04:04:02 +0200

    dior: Force run resize2fs in releasetools
    Fixes following error:
    
    about to run program [/tmp/install/bin/resize2fs_static] with 2 args
    resize2fs 1.44.4 (18-Aug-2018)
    Please run 'e2fsck -f /dev/block/platform/msm_sdcc.1/by-name/system' first.
    
    run_program: child exited with status 1
    
    Change-Id: I4ad6a99b3e0479c45556d9abb3c7f5f3c37ccfe2

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -3,5 +3,5 @@ import struct
 
 def FullOTA_PostValidate(info):
     info.script.AppendExtra('run_program("/tmp/install/bin/e2fsck_static", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
-    info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/platform/msm_sdcc.1/by-name/system");');
+    info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "-f", "/dev/block/platform/msm_sdcc.1/by-name/system");');
     info.script.AppendExtra('run_program("/tmp/install/bin/e2fsck_static", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
