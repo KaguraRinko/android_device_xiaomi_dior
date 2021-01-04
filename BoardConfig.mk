a8abeed5ff46a1785255cf1b07f7f7bf6b475848
baolong24 <1667375308@qq.com>
2021-01-04 09:57:07 +0800

    dior: Clean up tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,7 +133,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_dior
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/dior
-TARGET_KERNEL_CONFIG := mokee_dior_defconfig
+TARGET_KERNEL_CONFIG := dior_defconfig
 BOARD_KERNEL_CMDLINE := androidboot.bootdevice=msm_sdcc.1 console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=dior zcache.enabled=1 user_debug=31 msm_rtb.filter=0x37
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
